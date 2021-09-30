1266cb5bdefba1c8116801e29cff7813782e264e
Bruna Lima <brunaalimas@gmail.com>
2021-09-29 23:59:35 -0300

    first commit

---
+++ exerciciogustavo29.py
@@ -0,0 +1,77 @@
+#  1º Faça um programa que pergunta a idade do usuário e informe se ele está apto a votar ou não
+# idade = int(input('Qual a sua idade? '))
+# if (idade >= 18 and idade <70):
+#     print('Você tem',idade,"anos. Voto obrigatório!")
+# elif (idade>=16 and idade<18) or (idade>=70):
+#     print('Você tem',idade, 'anos. Voto opcional!')
+# elif idade < 16:
+#     print('Você tem',idade, 'anos. Voto inválido!')
+
+# 2º Faça um Programa que leia três números inteiros e mostre o maior deles.
+# n1 = int(input('Digite o primeiro número: '))
+# n2 = int(input('Digite o segundo número: '))
+# n3 = int(input('Digite o terceiro número: '))
+# maior = max(n1, n2, n3)
+# print(f'O maior número é {maior}')
+
+# 3º Faça um Programa que leia três números inteiros, em seguida mostre o maior e o menor deles.
+# one = int(input('Digite o primeiro número: '))
+# two = int(input('Digite o segundo número: '))
+# three = int(input('Digite o terceiro número: '))
+# maior = max(one, two, three)
+# menor = min(one, two, three)
+# print(f'O maior número é o {maior} e o menor é o {menor}.')
+
+# 4º Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+# n = 0
+# while n <= 100:
+#     print(n)
+#     n +=1
+
+# 5º Faça um programa, utilizando while, que mostre na tela de 0 até N, em que N é o limite inserido pelo usuário.
+# inicio = 0
+# limite = int(input('Digite um número: '))
+# while inicio <= limite:
+#     print(inicio)
+#     inicio = inicio + 1 
+
+# 6º Faça um programa, utilizando while e listas, que permita o usuário escrever o nome de cinco pessoas e os mostre na tela.
+# lista = []
+# i = 0
+# while i < 5:
+#     nome = (input('Digite o nome: '))
+#     lista.append(nome)
+#     i = i + 1
+# print(lista)
+
+#  7º Faça um programa, utilizando while, que permita o usuário fazer contas de adição enquanto quiser.
+# primeiro = int(input('Primeiro valor: '))
+# segundo = int(input('Segundo valor: '))
+# soma = primeiro + segundo
+# print(soma)
+# pergunta = input('Deseja rodar novamente? [S/N] ')
+# pergunta = 'S'
+# while pergunta == 'S':
+#     n = int(input('Primeiro valor: '))
+#     m = int(input('Segundo valor: '))
+#     soma = n + m
+#     r = str(input('Quer continuar? '))
+#     print('Fim')
+ 
+
+# 8º Faça um programa, utilizando while e listas, que permita o usuário realizar o cadastro de um número indeterminado
+# de pessoas enquanto quiser e os mostre na tela ao finalizar.
+# listas = []
+
+# while True:
+#     nome = (input('Digite o nome: '))
+#     listas.append(nome)
+#     if nome == 'sair':
+#         listas.remove('sair')
+#         print(listas)
+#         break
+
+# 9º Faça um programa que imprima a quantidade de números pares entre dois números da sua escolha.
+
+
+# 10º Faça um programa que imprima a soma de todos os números pares entre dois números da sua escolha.
