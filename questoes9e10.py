ce78c5ad7c08c8064fa671f03f33cfc34cd1b79f
Bruna Lima <brunaalimas@gmail.com>
2021-09-30 01:36:53 -0300

    inclusao das questoes 9 e 10

---
+++ questoes9e10.py
@@ -0,0 +1,25 @@
+
+# 9º Faça um programa que imprima a quantidade de números pares entre dois números da sua escolha.
+
+# a = int(input('Digite um número: '))
+# b = int(input('Digite um número: '))
+# contador = 0
+# while a <= b:
+#     if (a%2) == 0:
+#         contador = contador + 1
+#     a = a + 1
+# print(contador)
+
+# 10º Faça um programa que imprima a soma de todos os números pares entre dois números da sua escolha.
+# listas1 = []
+# a = int(input('Digite um número: '))
+# b = int(input('Digite um número: '))
+# contador = 0
+# while a <= b:
+#     if a%2 == 0:
+#         listas1.append(a)
+#     a = a + 1    
+    
+# somalista = sum(listas1)
+# print(listas1)
+# print(f'Os números pares somam {somalista}')
